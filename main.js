10c29f33015ab65214221ff424f229a18c095dde
Yurii <yuskornyakov@gmail.com>
2015-05-01 13:31:55 +0300

    Added exercise Work with variables

--- main.js
+++ main.js
@@ -2,4 +2,14 @@
 * Сделайте страницу, которая выводит «Я – JavaScript!».
 */
 
-alert("Я – JavaScript!");
+//alert("Я – JavaScript!");
+
+/*
+ * Работа с переменными
+ * */
+
+var admin;
+var name;
+name = 'Василий';
+admin = name;
+alert(admin);
